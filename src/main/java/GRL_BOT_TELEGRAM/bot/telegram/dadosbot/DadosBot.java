ac654ac436780ce26d2ea95314ae7d9f0bae7ead
MattDev2024 <matheus.ram.lima@gmail.com>
2024-09-04 06:52:49 -0300

    344

--- src/main/java/GRL_BOT_TELEGRAM/bot/telegram/dadosbot/DadosBot.java
+++ src/main/java/GRL_BOT_TELEGRAM/bot/telegram/dadosbot/DadosBot.java
@@ -1,6 +1,6 @@
 package GRL_BOT_TELEGRAM.bot.telegram.dadosbot;
 
 public class DadosBot {
-    public static final String BOT_TOKEN = "7361945839:AAErxRu-I-HbXkhSXzqvROz7_q2YBXwty-8";
-    public static final String BOT_USER_NAME = "Javis_matias_bot";
+    public static final String BOT_TOKEN = "";
+    public static final String BOT_USER_NAME = "";
 }
